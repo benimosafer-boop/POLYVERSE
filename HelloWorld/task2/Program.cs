cd30b19aa8739849233f22181c5ebca686534498
BenyaminMosafer <beni.mosafer@gmail.com>
2025-12-09 17:01:01 +0330

    Q4

---
+++ HelloWorld/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Teacher: AsherGhelich SeyedMohammadi
+Course Title: POLYVERSE
+Session: 4
+Session Title: C# Basic Programming System is Hacked lol
+Date: 2025-12-09
+GitHub: Benimosafer=boop
+*/
+
+string commander = "      KAKAJAN    System is Hacked ";
+commander = commander.Trim();
+commander = commander.Replace("System is Hacked", "System is Secure");
+Console.WriteLine(commander);
+bool containsSecure = commander.Contains("System is Secure");
+Console.WriteLine(containsSecure);
+int Bullets = 2140000000;
+int foundBulletsCount = 2140000000;
+double a = 12.5;
+float b = 5.02f;
+Console.WriteLine(a / b);
+int totalBullets = checked(Bullets + foundBulletsCount);
+Console.WriteLine(totalBullets);
