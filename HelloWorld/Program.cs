e35b65557efb5038de3fcd1f9ed4ce4febe1c495
BenyaminMosafer <beni.mosafer@gmail.com>
2025-12-09 14:10:14 +0330

    se03

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Benyamin mosafer!");
+
+Console.WriteLine("hello,Pasha");
+
+string greeting = ("Welcome to the world of c# programming");
+Console.WriteLine(greeting);
+
+string friendone = "beni";
+string friendtwo = "arsam";
+
+Console.WriteLine($"my friends are {friendone} and {friendtwo}.");
+
+
+
