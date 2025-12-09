8619d620c74b2854b65f34db8f28995ea407a115
BenyaminMosafer <beni.mosafer@gmail.com>
2025-12-09 16:03:03 +0330

    Q4

---
+++ HelloWorld/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-Console-template for more information
+string gametitle = "Forza 5";
+string carname = "BMW 2022";
+string carmod = "Rally";
+string currentmission = "Car Race in baku 58 lab";
+Console.WriteLine("************************");
+Console.WriteLine(gametitle);
+Console.WriteLine("************************");
+Console.WriteLine("");
+Console.WriteLine("LOADING PLAYER PROFILE...");
+Console.WriteLine("");
+Console.WriteLine("Car Name :" + carname);
+Console.WriteLine("");
+Console.WriteLine("Car Mod :" + carmod);
+Console.WriteLine("");
+Console.WriteLine("Race Mod : " + currentmission);
+Console.WriteLine("");
+Console.WriteLine("************************");
+
+
